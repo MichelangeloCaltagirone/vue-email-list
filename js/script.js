601a99a91895d298223cc3aa6c7fd99c0fb7a8cc
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-09-11 16:43:09 +0200

    axios Api call problem

--- js/script.js
+++ js/script.js
@@ -1,7 +1,33 @@
-console.log('ciao');
 
 const {createApp} = Vue;
 
 createApp({
-    
+
+    data: () => {
+        return {
+            apiUrl: "https://flynn.boolean.careers/exercises/api/random/mail",
+            email: ''
+        }
+    },
+
+    methods: {
+        getRandomEmail(){
+            axios.get(this.apiUrl).then(function(response) {
+            console.log(response.data.response),
+            this.email = response.data.response
+            })
+        }
+    },
+    mounted(){
+        this.getRandomEmail();
+        console.log(this.email, "è l'email in dataVue")
+    }
+}).mount('#emailNodeList')
+
+
+/*
+axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+.then(function(response) {
+    console.log(response.data.response)
 })
+*/
